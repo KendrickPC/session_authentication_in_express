f81c1ff5b471360af65b043c03a0b6e600c4afca
kendrickPC <kennethpchang@gmail.com>
2021-11-11 23:22:07 -0800

    project complete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const TWO_HOURS = 1000 * 60 * 60 * 2;
 
@@ -13,6 +14,7 @@ const {
 
 const IN_PRODUCTION = NODE_ENV === 'production'
 
+// TO DO DB 
 const users = [
   { id: 1,
     name: 'Alex',
@@ -32,6 +34,9 @@ const users = [
 ];
 
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true,
+}))
 
 app.use(session({
   name: SESSION_NAME,
@@ -46,8 +51,34 @@ app.use(session({
 }))
 
 
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/login')
+  } else {
+    next();
+  }
+}
+
+const redirectHome = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/home')
+  } else {
+    next();
+  }
+}
+
+app.use((req, res, next) => {
+  const { userId } = req.session
+  if (userId) {
+    res.locals.user =users.find(
+      user => user.id === user.id
+    )
+  }
+  next()
+})
+
 app.get('/', (req, res) => {
-  const userId = null;
+  const {userId} = req.session;
   
   
   res.send(`
@@ -66,18 +97,24 @@ app.get('/', (req, res) => {
   `)
 })
 
-app.get('/home', (req, res) => {
+app.get('/home', redirectLogin, (req, res) => {
+  const {user} = res.locals;
+  console.log(req.session);
   res.send(`
     <h1>Home</h1>
     <a href='/'>Main</a>
     <ul>
-      <li>Name: </li>
-      <li>Email: </li>
+      <li>Name: ${user.name} </li>
+      <li>Email: ${user.email} </li>
     </ul>
   `)
 })
 
-app.get('/login', (req, res) => {
+app.get('/profile', redirectLogin, (req, res) => {
+  const user = res.locals;
+})
+
+app.get('/login', redirectHome, (req, res) => {
   res.send(`
     <h1>Login</h1>
     <form method='post' action='/login'>
@@ -89,7 +126,7 @@ app.get('/login', (req, res) => {
   `)
 })
 
-app.get('/register', (req, res) => {
+app.get('/register', redirectHome, (req, res) => {
   res.send(`
     <h1>Register</h1>
     <form method='post' action='/register'>
@@ -99,11 +136,56 @@ app.get('/register', (req, res) => {
       <input type='submit' />
     </form>
     <a href='/login'>Login</a>
-`)
+  `)
 })
 
-app.get('/logout', (req, res) => {
-  
+app.post('/login', redirectHome, (req, res) => {
+  const { email, password } = req.body
+
+  if (email && password) {
+    const user = users.find(user => user.email === email && user.password === password)
+    if (user) {
+      req.session.userId = user.id
+      return res.redirect('/home')
+    }
+  }
+
+  res.redirect('/login')
+})
+
+app.post('/register', redirectHome, (req, res) => {
+  const { name, email, password } = req.body
+  if (name && email && password ) {
+    // todo validation
+    const exists = users.some(
+      user => user.email === email 
+    )
+    if (!exists) {
+      const user = {
+        id: user.length + 1,
+        name,
+        email,
+        password, // TODO hash
+      }
+      users.push(user)
+
+      req.session.userId = user.id
+
+      return res.redirect('/home')
+    }
+  }
+
+  res.redirect('/register') // todo qs errors
+})
+
+app.post('/logout', redirectLogin, (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.redirect('/home')
+    }
+    res.clearCookie(SESSION_NAME)
+    res.redirect('/login')
+  })
 })
 
 app.listen(PORT, () => console.log(
